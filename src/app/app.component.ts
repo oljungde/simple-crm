f50044d30c9872e32e06a000c69d3945bb4b0e98
oljungde <oljung@posteo.de>
2023-05-13 15:11:00 +0200

    project setup with local fonts and prepare for theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-crm';
+  isLightTheme = true;
 }
