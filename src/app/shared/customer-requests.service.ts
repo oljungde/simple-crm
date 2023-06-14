ab7abf67e0b4aecd41882a6906c5d46923eb26f7
oljungde <oljung@posteo.de>
2023-06-14 16:46:31 +0200

    customer requests service

--- src/app/shared/customer-requests.service.ts
+++ src/app/shared/customer-requests.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { CustomerRequest } from '../models/customer-request.class';
-import { DocumentReference, Firestore, addDoc, collection, updateDoc } from '@angular/fire/firestore';
+import { DocumentReference, Firestore, addDoc, collection, onSnapshot, updateDoc, where } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { query } from '@angular/animations';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,26 @@ export class CustomerRequestsService {
   customerRequestId: string = '';
   firestore: Firestore = inject(Firestore);
   customerRequestsCollection = collection(this.firestore, 'customerRequests');
+  allCustomerRequests$ = new BehaviorSubject<any[]>([]);
+  allCustomerRequests = this.allCustomerRequests$.asObservable();
 
-  constructor() { }
+  constructor() {
+    this.observeCustomerRequests();
+  }
+
+
+  observeCustomerRequests() {
+    onSnapshot(this.customerRequestsCollection, (changes) => {
+      let customerRequests: any = [];
+      changes.forEach((change) => {
+        let customerRequestData = change.data();
+        customerRequestData['id'] = change.id;
+        customerRequests.push(customerRequestData);
+      });
+      this.allCustomerRequests$.next(customerRequests);
+      console.log('Recieved customer request changes: ', this.allCustomerRequests$.value);
+    });
+  }
 
 
   saveNewCustomerRequest(customerRequest?: CustomerRequest) {
@@ -26,5 +46,22 @@ export class CustomerRequestsService {
         updateDoc(docRef, { customerRequestId: docRef.id });
         this.loading = false;
       });
+  }
+
+
+  getCustomerRequests(customerRef: string): any {
+    return new Observable(observer => {
+      const customerRequestsQuery = query(this.customerRequestsCollection, where('customerRef', '==', customerRef));
+      const unsubscribe = onSnapshot(customerRequestsQuery, (querySnapshot) => {
+        let customerRequestsData: any = [];
+        querySnapshot.forEach((customerRequest: any) => {
+          let customerRequestData = customerRequest.payload.doc.data();
+          customerRequestData['id'] = customerRequest.payload.doc.id;
+          customerRequestsData.push(customerRequestData);
+        });
+        observer.next(customerRequestsData);
+      });
+      return unsubscribe;
+    });
   }
 }
