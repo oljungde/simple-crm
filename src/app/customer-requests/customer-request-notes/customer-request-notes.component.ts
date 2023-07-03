bc5f6ca52ca85a9ba558768e39f6d3c2f3589863
oljungde <oljung@posteo.de>
2023-07-03 19:55:12 +0200

    bugfix show right unser name in customer request note

--- src/app/customer-requests/customer-request-notes/customer-request-notes.component.ts
+++ src/app/customer-requests/customer-request-notes/customer-request-notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-customer-request-notes',
@@ -7,16 +8,18 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CustomerRequestNotesComponent implements OnInit {
   @Input() note: string = '';
-  @Input() user: string = '';
+  @Input() userRef: string = '';
   @Input() date: number | undefined;
+  userFullName: string = '';
   noteDateCreated: string = '';
   
-  constructor() { }
+  constructor(private userService: UserService) { }
 
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     if (this.date) {
-      this.noteDateCreated = new Date(this.date).toLocaleDateString();
+      this.noteDateCreated = new Date(this.date).toLocaleString('de-DE', { dateStyle: 'short', timeStyle: 'short' });
     }
+    this.userFullName = await this.userService.getUserFullNameById(this.userRef);
   }
 }
